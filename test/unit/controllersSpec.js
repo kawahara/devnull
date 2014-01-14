b7161fc6a9ab77efd7c0e7a0629a25149e492186
Shogo Kawahara <shogo.a.kawahara@mail.rakuten.com>
2014-01-14 14:55:03 +0900

    Add test for the reply feature

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -17,14 +17,18 @@ describe('posts controllers', function() {
     var postsData = [{
           body: 'test',
           _id: '123',
-          replies: [],
+          replies: [{
+            _id: '456',
+            body: "test"
+          }],
           updated: new Date()
         }];
 
     beforeEach(inject(function(_$httpBackend_,$rootScope, $controller) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('api/posts').
-        respond(postsData);
+
+      $httpBackend.expectGET('api/posts')
+        .respond(200, postsData);
 
       scope = $rootScope.$new();
       ctrl = $controller('postsCntl', {$scope: scope});
@@ -34,6 +38,62 @@ describe('posts controllers', function() {
       expect(scope.posts).toEqualData([]);
       $httpBackend.flush();
       expect(scope.posts).toEqualData(postsData);
+    });
+
+    it('should display a new post after the submit action', function() {
+      var submitData = {
+        body: "Hello World."
+      };
+
+      $httpBackend.expectPOST('api/posts')
+        .respond(201, "");
+      scope.post = submitData;
+      scope.submit();
+      $httpBackend.flush();
+
+      expect(scope.post).toEqualData(null);
+      expect(scope.posts[0]).toEqualData(submitData);
+    });
+
+    it('should remove a post', function() {
+      $httpBackend.flush();
+      $httpBackend.expectDELETE('api/posts/123')
+        .respond(204, "");
+
+      scope.delete(scope.posts[0]);
+
+      $httpBackend.flush();
+
+      expect(scope.posts).toEqualData([]);
+    });
+
+    it('should display a new reply after the submit reply action', function() {
+      var replyData = {
+        body: "Hi, this is new reply"
+      };
+
+      $httpBackend.flush();
+      $httpBackend.expectPOST('api/posts/123/reply')
+        .respond(201, "");
+
+      scope.posts[0].newreply = replyData;
+      scope.reply(scope.posts[0]);
+      $httpBackend.flush();
+
+      expect(scope.posts[0].newreply).toEqualData(null);
+      expect(scope.posts[0].replies[1]).toEqualData(replyData);
+    });
+
+    it('should remove a reply', function() {
+      $httpBackend.flush();
+
+      $httpBackend.expectDELETE('api/posts/123/reply/456')
+        .respond(204, "");
+      scope.deleteReply(scope.posts[0], scope.posts[0].replies[0]);
+
+      $httpBackend.flush();
+
+      expect(scope.posts[0].replies).toEqualData([]);
     });
   });
 });
